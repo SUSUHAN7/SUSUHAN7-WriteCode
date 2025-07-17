36821a14771b72d1fa787fe2665d626ebd2fa5a1
SUSUHAN7 <kimjeongeun43@gmail.com>
2025-07-17 20:13:47 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < n - i; j++) {
+			printf(" ");
+		}
+		for (int k = 2 * i - 1; k > 0; k--) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
