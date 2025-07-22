5b149dbe08ce1265d09687d240ad90fed7b54f72
SUSUHAN7 <kimjeongeun43@gmail.com>
2025-07-22 23:21:47 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#pragma warning(disable : 4996)
+
+int main()
+{
+    int num1, num2;
+
+    while (1)
+    {
+        scanf("%d %d", &num1, &num2);
+
+        if (num1 == 0 && num2 == 0)
+        {
+            break;
+        }
+
+        else if (num2 % num1 == 0)
+        {
+            printf("factor\n");
+        }
+        else if (num1 % num2 == 0)
+        {
+            printf("multiple\n");
+        }
+        else
+        {
+            printf("neither\n");
+        }
+    }
+    return 0;
+}
